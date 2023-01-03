49ff642d9d2120e610fe427437011eb63ca5fd48
Robind Kumar <kumarrobind386@gmail.com>
2023-01-03 22:46:03 +0530

    Navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="nav-container">
+        <ul>
+            <li className="nav-item nav-item1">Name combiner</li>
+            <li className="nav-item nav-item2">2 Names</li>
+            <li className="nav-item nav-item3">3 Names</li>
+            <li className="nav-item nav-item3">4 Names</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
