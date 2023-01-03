f9877c939f2e2fec3defca888696b6dc34e6eb03
Robind Kumar <kumarrobind386@gmail.com>
2023-01-03 23:18:13 +0530

    Footer updated

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="footer-container">
+        <div className="footer-item footer-left">Left</div>
+        <div className="footer-item footer-mid">Mid</div>
+        <div className="footer-item footer-right">right</div>
+      </div>
+    </div>
+  )
+}
